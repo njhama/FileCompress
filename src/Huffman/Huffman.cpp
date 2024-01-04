5e00e0af5ee0074e4fce99000b0c58cd1439300a
Nicky Hama <njhama@usc.edu>
2024-01-04 14:42:28 -0800

    huffman

--- src/Huffman/Huffman.cpp
+++ src/Huffman/Huffman.cpp
@@ -1,1 +1,78 @@
-//imlpment
+#include "Huffman.h"
+
+HuffmanNode::HuffmanNode(char data, unsigned freq, HuffmanNode* left, HuffmanNode* right) 
+    : data(data), freq(freq), left(left), right(right) {}
+
+HuffmanNode::~HuffmanNode() {
+    delete left;
+    delete right;
+}
+
+bool Compare::operator()(HuffmanNode* left, HuffmanNode* right) {
+    return left->freq > right->freq;
+}
+
+HuffmanCoding::HuffmanCoding() : root(nullptr) {}
+
+HuffmanCoding::~HuffmanCoding() {
+    deleteTree(root);
+}
+
+std::string HuffmanCoding::compress(const std::string& text) {
+    std::unordered_map<char, unsigned> freq;
+    for (char ch : text) {
+        freq[ch]++;
+    }
+
+    std::priority_queue<HuffmanNode*, std::vector<HuffmanNode*>, Compare> pq;
+    for (auto pair : freq) {
+        pq.push(new HuffmanNode(pair.first, pair.second));
+    }
+
+    while (pq.size() != 1) {
+        HuffmanNode* left = pq.top(); pq.pop();
+        HuffmanNode* right = pq.top(); pq.pop();
+        pq.push(new HuffmanNode('\0', left->freq + right->freq, left, right));
+    }
+
+    root = pq.top();
+
+    printCodes(root, "");
+
+    std::string str;
+    for (char ch : text) {
+        str += huffmanCode[ch];
+    }
+
+    return str;
+}
+
+std::string HuffmanCoding::decompress(const std::string& binaryStr) {
+    std::string str;
+    HuffmanNode* current = root;
+    for (int i = 0; i < binaryStr.size(); i++) {
+        current = binaryStr[i] == '0' ? current->left : current->right;
+        if (!current->left && !current->right) {
+            str += current->data;
+            current = root;
+        }
+    }
+    return str;
+}
+
+void HuffmanCoding::printCodes(HuffmanNode* root, std::string str) {
+    if (!root) return;
+    if (root->data != '\0') {
+        huffmanCode[root->data] = str;
+        reverseHuffmanCode[str] = root->data;
+    }
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
+}
+
+void HuffmanCoding::deleteTree(HuffmanNode* node) {
+    if (node == nullptr) return;
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
--- src/Huffman/Huffman.h
+++ src/Huffman/Huffman.h
@@ -1,1 +1,40 @@
-//header
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <string>
+
+class HuffmanNode {
+public:
+    char data;
+    unsigned freq;
+    HuffmanNode *left, *right;
+    HuffmanNode(char data, unsigned freq, HuffmanNode* left = nullptr, HuffmanNode* right = nullptr);
+    
+    ~HuffmanNode(); 
+};
+
+class Compare {
+public:
+    bool operator()(HuffmanNode* left, HuffmanNode* right);
+};
+
+class HuffmanCoding {
+public:
+    HuffmanCoding();
+    ~HuffmanCoding();
+
+    std::string compress(const std::string& text);
+    std::string decompress(const std::string& binaryStr);
+
+private:
+    void printCodes(HuffmanNode* root, std::string str);
+    void deleteTree(HuffmanNode* node);
+    HuffmanNode* root;
+    std::unordered_map<char, std::string> huffmanCode;
+    std::unordered_map<std::string, char> reverseHuffmanCode;
+};
+
+#endif 
